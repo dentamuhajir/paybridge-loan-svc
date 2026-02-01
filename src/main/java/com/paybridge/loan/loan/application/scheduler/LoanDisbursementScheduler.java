a8d4adb2e03c41ee0be5634baea6ad1bd222dacd
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-02-01 16:29:03 +0700

    feat(scheduler): enable & test schedular

---
+++ src/main/java/com/paybridge/loan/loan/application/scheduler/LoanDisbursementScheduler.java
@@ -0,0 +1,23 @@
+package com.paybridge.loan.loan.application.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LoanDisbursementScheduler {
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void runAutoDisbursement() {
+        log.info("[Scheduler] Starting auto disbursement job");
+
+        try {
+            // autoDisbursementUseCase.execute();
+        } catch (Exception e) {
+            log.error("[Scheduler] Auto disbursement job failed", e);
+        }
+
+        log.info("[Scheduler] Finished auto disbursement job");
+    }
+}
